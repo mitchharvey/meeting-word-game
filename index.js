d43dd70143f080ac5530cd50b3e0077caa938292
Mitchell Harvey <mph121@users.noreply.github.com>
2020-01-29 23:44:57 -0500

    prevent two clicks at same time, skipping a word

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const NUM_PLAYERS = 3
 const NUM_WORDS = 4
 
 let wordlist = shuffle(require('./wordlist.json'))
-let currlist = wordlist.slice(0, NUM_WORDS * NUM_PLAYERS)
+const currlist = wordlist.slice(0, NUM_WORDS * NUM_PLAYERS)
 wordlist = wordlist.slice(NUM_WORDS * NUM_PLAYERS)
-let scores = new Array(NUM_PLAYERS).fill(0)
-let names = new Array(NUM_PLAYERS).fill("")
-
+const scores = new Array(NUM_PLAYERS).fill(0)
+const names = new Array(NUM_PLAYERS).fill('')
 
 // Add JS sources
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
@@ -29,37 +28,37 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-io.on('connection', function(socket) {
-  const shortid = socket.id.substr(0,3)
+io.on('connection', function (socket) {
+  const shortid = socket.id.substr(0, 3)
   console.log('CONN: ' + shortid)
 
   // Give client initial info
   socket.emit('NEW list', currlist)
   socket.emit('NEW scores', scores)
   for (const num in names) {
-    socket.emit('NEW name', names[num], "name"+num)
+    socket.emit('NEW name', names[num], 'name' + num)
   }
 
-  socket.on('disconnect', function() {
-    console.log('DSCN: ' + shortid);
-  });
+  socket.on('disconnect', function () {
+    console.log('DSCN: ' + shortid)
+  })
 
-  socket.on('REQ list', function() {
-    console.log('REQ list: ' + shortid);
+  socket.on('REQ list', function () {
+    console.log('REQ list: ' + shortid)
     socket.emit('NEW list', currlist)
-  });
+  })
 
-  socket.on('DEL word', function(id) {
+  socket.on('DEL word', function (id) {
     // Get new word and send out
     console.log('DEL word: ' + shortid + ' ' + id)
-    let num = parseInt(id.substr(4))
-    let newword = wordlist.shift()
+    const num = parseInt(id.substr(4))
+    const newword = wordlist.shift()
     currlist[num] = newword
     console.log('NEW word: ' + newword + ' ' + id)
     io.emit('NEW word', newword, id)
 
     // Increase score
-    let playerNum = Math.floor(num / NUM_WORDS)
+    const playerNum = Math.floor(num / NUM_WORDS)
     scores[playerNum]++
     console.log('INC score: ' + playerNum + ' ' + scores[playerNum])
     io.emit('NEW scores', scores)
@@ -67,7 +66,7 @@ io.on('connection', function(socket) {
 
   socket.on('REQ name', function (name, id) {
     console.log('REQ name: ' + shortid + ' ' + name + ' ' + id)
-    let num = parseInt(id.substr(4))
+    const num = parseInt(id.substr(4))
     names[num] = name
     socket.broadcast.emit('NEW name', name, id)
   })
--- js/client.js
+++ js/client.js
@@ -1,29 +1,29 @@
 const socket = io()
 /* Events */
 // Receive new word list
-socket.on('NEW list', function(list) {
+socket.on('NEW list', function (list) {
   insertWords(list)
 })
 
 // Receive new word
-socket.on('NEW word', function(word, id) {
+socket.on('NEW word', function (word, id) {
   insertWord(word, id)
 })
 
 // Receive new score
-socket.on('NEW scores', function(scores) {
+socket.on('NEW scores', function (scores) {
   insertScores(scores)
 })
 
-socket.on('NEW name', function(name, id) {
+socket.on('NEW name', function (name, id) {
   insertName(name, id)
 })
 
 /* Change DOM in response to events */
 // Insert words into page
-function insertWords(list) {
+function insertWords (list) {
   // Clear current wordlists
-  $(".wordslist").empty()
+  $('.wordslist').empty()
   const numPerCol = list.length / 3
   for (const word in list) {
     if (list[word] === null) {
@@ -31,61 +31,68 @@ function insertWords(list) {
     }
 
     // Add to correct list
-    let listNum = Math.floor(word/numPerCol)
-    let li = $("#list" + listNum)
+    const listNum = Math.floor(word / numPerCol)
+    const li = $('#list' + listNum)
     li.append(`<div class="word" id="word${word}">${list[word]}</div>`)
   }
 
   // Setup Listener
-  $(".word").on('click', function() {
+  $('.word').on('click', function () {
+    if ($(this).attr('disabled')) {
+      return false
+    }
     const id = this.id
     delWord(id)
   })
 }
 
 // Replace word at location
-function insertWord(word, id) {
-  console.log("NEW word: " + word + ' ' + id)
+function insertWord (word, id) {
+  console.log('NEW word: ' + word + ' ' + id)
   if (word === null) {
-    $("#"+id).fadeOut()
+    $('#' + id).fadeOut()
     return
   }
+  const div = $('#' + id)
   // Cannot pass to callback functions, so store next word as an attribute.
-  $("#"+id).attr("next", word)
-  $("#"+id).fadeTo(750, 0, function(word) {
-    $(this).text($(this).attr("next"))
-    $(this).fadeTo(750, 1)
+  div.attr('disabled', true)
+  div.attr('next', word)
+  div.fadeTo(750, 0, function () {
+    $(this).text($(this).attr('next'))
+    $(this).fadeTo(750, 1, function () {
+      $(this).removeAttr('disabled')
+    })
   })
 }
 
 // Replace score at location
-function insertScores(scores) {
-  console.log("NEW scores: " + scores)
+function insertScores (scores) {
+  console.log('NEW scores: ' + scores)
   for (const num in scores) {
-    $("#score"+num).text(scores[num])
+    $('#score' + num).text(scores[num])
   }
 }
 
 // Replace name
-function insertName(name, id) {
-  console.log("NEW name: " + name + ' ' + id)
-  $("#"+id).val(name)
+function insertName (name, id) {
+  console.log('NEW name: ' + name + ' ' + id)
+  $('#' + id).val(name)
 }
 
 // Request new word list
-function loadList() {
+function loadList () {
   socket.emit('REQ list')
 }
 
 // Request word is removed
-function delWord(id) {
+function delWord (id) {
   socket.emit('DEL word', id)
 }
 
 $(document).ready(() => {
-  $(".name input").change(function () {
-    let id = $(this).attr('id')
-    let val = $(this).val()
+  $('.name input').change(function () {
+    const id = $(this).attr('id')
+    const val = $(this).val()
     console.log('REQ name', val, id)
     socket.emit('REQ name', val, id)
   })
